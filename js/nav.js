8184460bfd811e0b2e3834af1fc47038671cb672
kimi1723 <100174940+kimi1723@users.noreply.github.com>
2023-01-17 01:27:04 +0100

    project

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,19 @@
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
+const navBg = document.querySelector('.nav-bg');
+
+const onScroll = () => {
+    if( document.documentElement.scrollTop > 80) {
+        navBg.style.display = 'block';
+    } else {
+        navBg.style.display = 'none';
+    }
+}
 
 const show = () => {
     menu.classList.toggle('visible');
 }
 
+
 burger.addEventListener('click', show);
+window.addEventListener('scroll', onScroll);
